ee9dde1b9109da51ed06c8db7889f71c0c78034f
ixh144 <59976345+ixh144@users.noreply.github.com>
2020-04-01 15:01:58 -0400

    123

--- jQuery/JQueryA.js
+++ jQuery/JQueryA.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   $(".final").show();
+  $(".step").hide();
   $(".Step1").hide();
   $(".Step2").hide();
   $(".Step3").hide();
@@ -8,70 +9,81 @@ $(document).ready(function(){
   $(".Step6").hide();
 
 
-
   $("#step1").click(function(){
     $(".final").hide();
+    $(".step").show();
     $(".Step1").show();
     $(".Step2").hide();
     $(".Step3").hide();
     $(".Step4").hide();
     $(".Step5").hide();
     $(".Step6").hide();
-
+    $("#step1").addClass("active");
   });
 
   $("#step2").click(function(){
     $(".final").hide();
+    $(".step").show();
     $(".Step1").hide();
     $(".Step2").show();
     $(".Step3").hide();
     $(".Step4").hide();
     $(".Step5").hide();
     $(".Step6").hide();
+    $("#step1").removeClass("active");
+    $("#step2").addClass("active");
 
   });
 
   $("#step3").click(function(){
     $(".final").hide();
+    $(".step").show();
     $(".Step1").hide();
     $(".Step2").hide();
     $(".Step3").show();
     $(".Step4").hide();
     $(".Step5").hide();
     $(".Step6").hide();
+    $("#step3").addClass("active");
 
 
   });
   $("#step4").click(function(){
     $(".final").hide();
+    $(".step").show();
     $("main .1").hide();
     $("main .2").hide();
     $("main .3").hide();
     $("main .4").show();
     $("main .5").hide();
     $("main .6").hide();
+    $("#step4").addClass("active");
 
 
   });
   $("#step5").click(function(){
     $(".final").hide();
+    $(".step").show();
     $(".Step1").hide();
     $(".Step2").hide();
     $(".Step3").hide();
     $(".Step4").hide();
     $(".Step5").show();
     $(".Step6").hide();
+    $("#step5").addClass("active");
 
 
   });
   $("#step6").click(function(){
     $(".final").hide();
+    $(".step").show();
     $(".Step1").hide();
     $(".Step2").hide();
     $(".Step3").hide();
     $(".Step4").hide();
     $(".Step5").hide();
     $(".Step6").show();
+    $("#step6").addClass("active");
 
 
   });
